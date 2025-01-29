d5234cff0e3c64c7c5d70377b394804173aeca45
saikumar pujari <saikumarpujari10@gmail.com>
2025-01-29 20:13:52 +0530

    FIZZBUZZ code

---
+++ FIzzbuzz.py
@@ -0,0 +1,15 @@
+
+n=int(input())
+
+def fiiz(n):
+    for i in range(1,n+1):
+        if i % 3==0 and i % 5==0:
+            print("Fizzbuzz")
+        elif i % 3==0:
+            print("fizz")
+        elif i % 5==0:
+            print("buzz")
+        else:
+            print(i)
+
+fiiz(n)
